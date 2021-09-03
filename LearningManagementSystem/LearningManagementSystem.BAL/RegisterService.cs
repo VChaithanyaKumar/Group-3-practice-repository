8c2b62f9506941bf497b1ac1821a2e6d2675930e
Vakati Chaithanya Kumar Reddy <chaitu.vakati@gmail.com>
2021-09-03 13:05:52 +0530

    trythis

---
+++ LearningManagementSystem/LearningManagementSystem.BAL/RegisterService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using LMS.DAL;
+using LMS.BAL.Validations;
+using LearningManagementSystem.Entities;
+namespace LMS.BAL
+{
+    //Registration-- Josy George
+    public class RegisterService
+    {
+        //object reference for RegisterRepository
+        private RegisterRepository register = null;
+        string Re_Password;
+        public RegisterService(User user, string Re_Password)
+        {
+            this.Re_Password = Re_Password;
+            register = new RegisterRepository();
+        }
+        //invoking AddUser from the RegisterRepository
+        
+        public string RegisterUser(User user)
+        {
+            if (RegisterValidation.ValidatePassword(user.UserPassword)!=true)
+            {
+                return "A password must the Conditions.\n 1. Length should be between [8-15] \n 2. contains a Uppercase and a lower case Character \n 3. contains a number\nPlease Try Again.";  
+            }
+            if (RegisterValidation.CheckPassword(user.UserPassword, Re_Password))
+            {
+
+                register.AddingUser(user);
+                return "SucessfullyAdded!";
+            }
+            else
+                return "Password missmatch!";
+
+        }
+
+    }
+}
---
+++ LearningManagementSystem/LearningManagementSystem.UI/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using LMS.BAL;
+using LearningManagementSystem.Entities;
+using System.IO;
+namespace LearningManagementSystem.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RegisterService registerService = null;
+                LoginService loginService = null;
+                User user = null;
+                do
+                {
+                    Console.WriteLine("1. Existing User");
+                    Console.WriteLine("2. New User");
+                    Console.WriteLine("3. Exit");
+                    Console.WriteLine("Enter your choice");
+                    int ch = int.Parse(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case 1:
+                            {
+
+                                Console.WriteLine("1. Admin Login");
+                                Console.WriteLine("2. Learner Login");
+                                Console.WriteLine("3. Go Back");
+                                Console.WriteLine("Enter your choice");
+                                int ch1 = int.Parse(Console.ReadLine());
+                                loginService = new LoginService();
+                                switch (ch1)
+                                {
+                                    case 1:
+                                        {
+                                            user = new User();
+                                            user.UserType = "Admin";
+                                            Console.WriteLine("enter email");
+                                            user.UserEmail = Console.ReadLine();
+                                            Console.WriteLine("enter password");
+                                            user.UserPassword = Console.ReadLine();
+                                            string UserEmail = user.UserEmail;
+                                            Console.WriteLine(loginService.LoginUser(user));
+                                            break;
+                                        }
+                                    case 2:
+                                        {
+                                            user = new User();
+                                            user.UserType = "Learner";
+                                            Console.WriteLine("enter email");
+                                            user.UserEmail = Console.ReadLine();
+                                            Console.WriteLine("enter password");
+                                            user.UserPassword = Console.ReadLine();
+                                            string UserEmail = user.UserEmail;
+                                            Console.WriteLine(loginService.LoginUser(user));
+                                            break;
+                                        }
+                                    case 3:
+                                        {
+                                            continue;
+                                        }
+                                }
+
+                                break;
+                            }
+                        case 2:
+                            {
+
+                                user = new User();
+                                Console.WriteLine("Enter UserType");
+                                user.UserType = Console.ReadLine();
+
+                                Console.WriteLine("Enter first name");
+                                user.UserFirstName = Console.ReadLine();
+                                Console.WriteLine("Enter last name");
+                                user.UserLastName = Console.ReadLine();
+                                Console.WriteLine("Enter Gender");
+                                user.Gender = char.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Email");
+                                user.UserEmail = Console.ReadLine();
+                                Console.WriteLine("Enter password");
+                                user.UserPassword = Console.ReadLine();
+                                Console.WriteLine("Retype Password");
+                                string passwordRetype = Console.ReadLine();
+                                registerService = new RegisterService(user, passwordRetype);
+                                Console.WriteLine(registerService.RegisterUser(user));
+                                break;
+                            }
+                        case 3:
+                            {
+
+                                return;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Invalid choice");
+                                break;
+                            }
+                    }
+                } while (true);
+            }
+            catch (Exception e)
+            {
+                FileStream fileStream = new FileStream("G:\\MyCopyLMS\\LearningManagementSystem\\ErrorLog.txt", FileMode.OpenOrCreate);
+                string ErrorMsg= string.Format($"Error Message:{e.Message} Date:{DateTime.Now} Class:Programe");
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+                streamWriter.WriteLine(ErrorMsg);
+                streamWriter.Close();
+                fileStream.Close();
+
+            }
+        }
+    }
+}
